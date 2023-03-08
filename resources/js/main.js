b9fa27619cc462d60498af9bce4f9b2041456d48
Oscar <115230428+AveragePythonEnjoyer29@users.noreply.github.com>
2023-03-08 14:46:42 +0000

    Add files via upload

--- resources/js/main.js
+++ resources/js/main.js
@@ -3,12 +3,52 @@ const stopButton = document.getElementById("js--stopButton");
 const resetButton = document.getElementById("js--resetButton");
 const secondsDisplay = document.getElementById("js--seconds");
 const minutesDisplay = document.getElementById("js--minutes");
+const slider = document.getElementById("js--slider");
+const sliderText = document.getElementById("js--slider-text");
+const body = document.getElementById("js--body");
+const image = document.getElementById("js--imageView");
+const displayButton = document.getElementById("js--displayButton");
+
+function setFontSize(rem_size) {
+    slider.value = rem_size;
+    body.style.fontSize = `${slider.value}rem`;
+    sliderText.innerText = `${slider.value}x`;
+}
+
+setFontSize(2);
 
 let counterSec = 0; // seconds
 let counterMin = 0; // minutes
 
 let isRunning = false;
+let imageShown = false;
 let timer = null;
+
+let imageInfo = {
+    "alt": "afbeelding van een engineer uit TF2",
+    "src": "https://media.tenor.com/MV666zYV7X4AAAAC/tf2-tf2engineer.gif"
+};
+
+displayButton.onclick = function () {
+
+    if (!imageShown) {
+        image.setAttribute("src", imageInfo.src);
+        image.setAttribute("alt", imageInfo.alt);
+
+        displayButton.innerText = "Hide image"
+    } else {
+        image.setAttribute("src", "");
+        image.setAttribute("alt", "");
+
+        displayButton.innerText = "Display image"
+    }
+
+    imageShown = !imageShown; // big brain
+};
+
+slider.oninput = function() {
+    setFontSize(slider.value)
+};
 
 startButton.onclick = function () {
     if (!isRunning) {
@@ -25,7 +65,7 @@ startButton.onclick = function () {
             secondsDisplay.innerText = counterSec;
             minutesDisplay.innerText = counterMin;
 
-        }, 50)
+        }, 1000)
         
         isRunning = true; // set running to true
     }
